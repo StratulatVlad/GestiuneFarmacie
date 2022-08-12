4d921107479b46c2b1dd9a91101f488851b1adf0
Stratulat Vlad-Radu <vlad_vladis8@yahoo.com>
2022-08-12 19:33:06 +0300

    Task4 fail

--- src/main/java/com/example/gestiunefarmacie/Task4Controller.java
+++ src/main/java/com/example/gestiunefarmacie/Task4Controller.java
@@ -3,12 +3,62 @@ package com.example.gestiunefarmacie;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.sql.*;
 
 public class Task4Controller {
     @FXML
     private Button cancelButton;
+    @FXML
+    private TextField textAn;
+    @FXML
+    private Button afisareButton;
+    @FXML
+    private Label farmaLabel;
+    @FXML
+    private Label orasLabel;
+    public void afisareButtonOnAction(ActionEvent event) throws SQLException {
+        DBconnection dBconnection= new DBconnection();
+        Connection connection = dBconnection.getConnection();
 
+
+        PreparedStatement pr = null;
+        ResultSet queryResult = null;
+
+
+
+
+        //String sql = "SELECT f.cod,f.nume,c.cantitate_com,m.pret,sum((c.cantitate_com * m.pret)) as suma_absoluta  FROM Farmacii f, Medicamente m, Comenzi c where c.cod_med = m.cod_med and m.cod_farma = f.cod and c.data_livrare >= '"+textAn.getText()+"-01-01' and c.data_livrare <= '"+textAn.getText()+"-12-31' group by f.cod,f.nume, c.cantitate_com, m.pret";
+        //String sql = "Select f.nume,f.oras,sum(suma_comanda) as suma_absoluta from Farmacii f where suma_comanda in (Select (c.cantitate_com * m.pret) as suma_comanda from Medicamente m, Comenzi c where c.cod_med = m.cod_med)";
+
+        try{
+            Statement statement=connection.createStatement();
+            queryResult= statement.executeQuery(sql);
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            e.getCause();
+            throw new SQLException("SQL Select statemant returns null");
+
+        }
+        Integer max_suma = 0;
+        String max_farma = null;
+        while(queryResult.next()){
+            System.out.println(queryResult.getString("suma_absoluta")+" " + queryResult.getString("nume") +" "+ queryResult.getString("cod"));
+            if(max_suma <Integer.parseInt(queryResult.getString("suma_absoluta"))){
+                max_suma = Integer.parseInt(queryResult.getString("suma_absoluta"));
+                max_farma = queryResult.getString("nume");
+            }
+        }
+
+        //System.out.println(max_farma);
+
+
+    }
     public void cancelButtonOnAction(ActionEvent e){
         Stage stage=(Stage) cancelButton.getScene().getWindow();
         stage.close();
